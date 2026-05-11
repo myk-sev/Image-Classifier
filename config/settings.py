73ba96dfb62ee14422fe77595eac6fa4aeebce8d
Mykhaylo Severinov <sev.myk@outlook.com>
2026-05-11 14:25:30 -0500

    Fix: whitenoise middleware

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,6 +138,12 @@ STATIC_ROOT = BASE_DIR / "staticfiles"  # folder collectstatic copies files into
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 ### Image Handling ###
